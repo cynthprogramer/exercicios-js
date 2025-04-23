7472a8bae9395dc2d6043e6f2ae8f5acef9861f2
Cynthia Santiago <133544879+cynthprogramer@users.noreply.github.com>
2025-04-23 11:45:48 -0300

    refaça sua boco

--- manipulacao/script.js
+++
@@ -1,8 +0,0 @@
-function criar_limpar(){
-    let criando = document.getElementById('criar').value;
-    let limpar = document.getElementById('limpar').value;
-    let table = document.getElementById('tabelinha');
-    criar_limpar();
-
-    let tabela = document.createElement('table');
-}
