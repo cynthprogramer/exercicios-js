05500e2d25bd20d962440ff108e6327635b8e360
Cynthia Santiago <133544879+cynthprogramer@users.noreply.github.com>
2025-04-23 11:33:52 -0300

    as tabelinhas
    - o chat fez, tente vc sozinha!!!!

---
+++ manipulacao/script.js
@@ -0,0 +1,8 @@
+function criar_limpar(){
+    let criando = document.getElementById('criar').value;
+    let limpar = document.getElementById('limpar').value;
+    let table = document.getElementById('tabelinha');
+    criar_limpar();
+
+    let tabela = document.createElement('table');
+}
