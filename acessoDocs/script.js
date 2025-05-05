2dad16cad701199ede90540ca9aae7e2c7afef29
Cynthia Santiago <133544879+cynthprogramer@users.noreply.github.com>
2025-05-05 16:37:39 -0300

    desafio das tabelinhas
    nao consegui fazer, fiz só o html. oq eu tentei de js foi praticamente colando do codigo do professor :(((

--- acessoDocs/script.js
+++ acessoDocs/script.js
@@ -1,8 +1,29 @@
-function criar_limpar(){
-    let criando = document.getElementById('criar').value;
-    let limpar = document.getElementById('limpar').value;
-    let table = document.getElementById('tabelinha');
-    criar_limpar();
+function limpar(){
+    var divTabela = document.getElementByIdById('divTabela');
+    divTabela.innerHTML="";
+};
 
-    let tabela = document.createElement('table');
+function IniciarCriacaoTabela(){
+    limpar();
+    var numLinhas = parseInt(document.getElementById('linha').value)
+    var numColunas = parseInt(document.getElementById(coluna).value)
+    var tabela = criarTabela(numLinhas, numColunas);
+    var divTabela = document.getElementById('divTabela');
+    divTabela.appendChild(tabela);
+}
+
+function criarTabela(numLinhas, numColunas){
+    var tabela = document.createElement('table')
+    table.setAttribute('border', '1');
+	tabela = criarLinhas(table, numLinhas, numColunas);
+	return tabela;
+}
+
+function criarLinhas(table, numLinhas, numColunas){
+    for (var i = 1; i<numLinhas; i++){
+        var tr = document.createElement('tr');
+        tr = criarColuna(tr, numColunas);
+        tabela.appendChild(tr);
+    }
+    return tabela;
 }
