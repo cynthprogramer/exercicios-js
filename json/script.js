56cbc21c85873be77e781a5a5ff77f5d4a76e596
Cynthia Santiago <133544879+cynthprogramer@users.noreply.github.com>
2025-05-21 11:15:37 -0300

    JSON e API de clima

---
+++ json/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    // Ação do botão
+    $('#botao').click(function() {
+        var cidade = $('#fcidade').val();  // Pega o nome da cidade do input
+        if(cidade) {
+            // Faz a requisição para a API WTTR.IN
+            $.getJSON('https://wttr.in/{cidade}?format=j1', function(data) {
+                // Exibe os dados na página
+                var tempAtual = data.current_condition[0].temp_C; // Temperatura atual em °C
+                var sensacao = data.current_condition[0].FeelsLikeC;
+
+                // Cria e insere os elementos na página
+                $('body').append(`
+                    <div id="resultado">
+                        <h2>A cidade de ${cidade}</h2>
+                        <p>Está como temperatura de ${tempAtual} °C</p>
+                        <p>Com sensação termica de ${sensacao}°C</p>
+                        
+                    </div>
+                `);
+            }).fail(function() {
+                // Caso a cidade não seja encontrada ou a API falhe
+                alert("Não foi possível obter os dados da cidade. Verifique o nome da cidade.");
+            });
+        } else {
+            alert("Por favor, insira o nome da cidade.");
+        }
+    });
+});
